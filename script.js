01380a3848d9cc72d36db0b09a2dd6937f95408b
Zikoooz1990 <zikoozelzoz0@gmail.com>
2025-09-16 08:18:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,25 @@ function t(k){return translations[currentLang][k];}
 
 const schoolsData = [
   {name:"STEM High School – Maadi",address:"X876+FH9, Maadi as Sarayat Al Gharbeyah, Tura, Cairo Governorate 4064145",lat:29.963685859495783,lng:31.311470067023386},
-  /* … باقى المدارس هنا … */
+  {name:"STEM High School – Nasr City",address:"2CHG+4RM, Nasr City, Cairo Governorate 4731130",lat:30.02886071407647,lng:31.426460323294073},
+  {name:"STEM High School for Boys – 6th of October",address:"W3MF+64 October Gardens, First 6th of October, Giza Governorate 3283010",lat:29.93308155892515,lng:31.07267764983385},
+  {name:"STEM High School – Al Wegieh (6th of October)",address:"Al Wegieh Center, 6th of October City, Giza Governorate",lat:29.961936869971872,lng:30.902121195858626},
+  {name:"STEM High School – Borg El Arab (Alexandria)",address:"VH9P+HMM, Hod Sakrah WA Abu Hamad, Borg El Arab, Alexandria Governorate 5221250",lat:30.86896617451884,lng:29.5866984535659},
+  {name:"STEM High School – Luxor (Red Sea)",address:"PQP6+WFC, Tieba City Unnamed Road, Red Sea Governorate, Luxor City, Luxor",lat:25.736938426710903,lng:32.76149701843059},
+  {name:"STEM High School – Dakahlia",address:"CGPC+C43, International Coastal Rd, Al Hafir WA Al Amal, Belqas, Dakahlia Governorate 7730203",lat:31.436032328342286,lng:31.520290395918593},
+  {name:"STEM High School – Al-Sharqia (Zagazig)",address:"HGP9+XMW, Harayah Raznah, Zagazig, Al-Sharqia Governorate 7124105",lat:30.58748543870247,lng:31.519180038213037},
+  {name:"STEM High School – Kafr El-Sheikh",address:"El Drayeb Street, Qism Kafr El-Shaikh, Kafr Al Sheikh First, Kafr El-Sheikh Governorate 6860581",lat:31.11382951817345,lng:30.954409026587758},
+  {name:"STEM High School – Sers El Laian (Menofia)",address:"CXV5+8HH, Madinet SERS Al Layanah, Sers El Laian City, Menofia Governorate 6060084",lat:30.44331327472523,lng:30.959007953548515},
+  {name:"STEM High School – Sadat City (Menofia)",address:"9GJW+PM, Sadat City, Menofia Governorate 6011521",lat:30.3818088747554,lng:30.546622009369415},
+  {name:"STEM High School – Ismailia",address:"Security Forces Complex, Educational Complex, Ismailia Desert Rd, 41511",lat:30.62618167463584,lng:32.22689992472066},
+  {name:"STEM High School – Obour",address:"Obour, Al-Qalyubia Governorate 6360191",lat:30.2263347584288,lng:31.44888811490449},
+  {name:"STEM High School – Faiyum",address:"Behind New Social Housing Buildings, Al Faiyum Al Gadida City, Faiyum Governorate",lat:29.21625487535105,lng:30.868401953499752},
+  {name:"STEM High School – Assiut",address:"53PF+4XP, Assiut El Wady El Gedid Road, Al Bourah, Asyut, Assiut Governorate",lat:27.184882189095735,lng:31.074990066917273},
+  {name:"STEM High School – New Minya",address:"4V87+7RQ, Al Zahir Al Sahrawi, New Minya, Minya Governorate",lat:28.115702075950917,lng:30.864623995787007},
+  {name:"STEM High School – Beni Suef",address:"Unnamed Road, Third Neighborhood, Beni Suef New City, Beni Suef Governorate",lat:29.02065431190559,lng:31.099411524656915},
+  {name:"STEM High School – Hurghada (Red Sea)",address:"7PQJ+XCR, Mubarak 11, Street of STEM School, Hurghada 2, Red Sea Governorate, Egypt",lat:27.290545241784063,lng:33.73113602459167},
+  {name:"STEM High School – Sohag",address:"CMV8+4GG, Sohag Al Gadida City, Sohag Governorate, Egypt",lat:26.442810283308162,lng:31.66632969572585},
+  {name:"STEM High School – Qena",address:"Unnamed Road, New Qena City, Qena Governorate, Egypt",lat:26.244772222719977,lng:32.74068399571885},
   {name:"STEM High School – Tanta (Gharbia)",address:"Q2C8+MQJ, Kafr Abou Dawoud, Tanta, Gharbia Governorate, Egypt",lat:30.771719174565504,lng:31.016861338220636}
 ];
 
@@ -63,7 +81,7 @@ async function renderSchools(){
   schoolsData.sort((a,b)=>a.distance-b.distance);
   list.innerHTML='';
   schoolsData.forEach((s,i)=>{
-    const marker=L.marker([s.lat,s.lng]).addTo(window.map).bindPopup(`<b>${s.name}</b><br>${s.address}`);
+    L.marker([s.lat,s.lng]).addTo(window.map).bindPopup(`<b>${s.name}</b><br>${s.address}`);
 
     const card=document.createElement('div');
     card.className='school-item';
@@ -71,9 +89,9 @@ async function renderSchools(){
       <h3>${s.name}</h3>
       <p>${t('address')}: ${s.address}</p>
       <p id="dist-${i}">${t('distance')}: ${s.distance.toFixed(2)} km</p>
-      <a href="${s.gm_link}" target="_blank">${t('openMap')}</a>
-      <button id="drive-${i}">${t('driving')}</button>
-      <button id="route-${i}">${t('route')}</button>
+      <a href="${s.gm_link}" target="_blank">${t('openMap')}</a><br>
+      <button class="btn" id="drive-${i}"><i class="fa-solid fa-car"></i> ${t('driving')}</button>
+      <button class="btn" id="route-${i}"><i class="fa-solid fa-route"></i> ${t('route')}</button>
     `;
     list.appendChild(card);
 
